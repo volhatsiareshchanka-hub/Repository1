45d6390fde9491a37880f39c95e9592ac7b0daa2
Olga <volha.tsiareshchanka@gmail.com>
2025-11-19 16:16:10 -0500

    Initial commit

---
+++ GIT_MyFile.py
@@ -0,0 +1,10 @@
+name = 'Olga'
+age = 36
+
+print(name)
+print(name.upper())
+print(name.lower())
+print(name * 3)
+print(name + name)
+print("My Name is {}, I'm {} year old".format(name, age))
+print(f"My name is {name} and I'm {age} years old")
